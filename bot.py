b639c16a0709830c2b16d624917a0c168fecd49f
Andrew Mead <ammead@umass.edu>
2022-08-15 15:55:37 -0400

    Automatically load all mentions and add last loaded propmt stopping point

--- bot.py
+++ bot.py
@@ -1,9 +1,12 @@
-from selenium.webdriver import Chrome
+import argparse
+import time
+
+from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.action_chains import ActionChains
+from webdriver_manager.chrome import ChromeDriverManager
 from download_discord_file import download_discord_file
-import time
-import argparse
+
 
 parser = argparse.ArgumentParser(
     description='Download Stable Diffusion Images From Discord')
@@ -11,8 +14,8 @@ parser.add_argument('-E', '--email', type=str, required=True,
                     help='Email Associated With Your Discord Account')
 parser.add_argument('-P', '--password', type=str,
                     required=True, help='Your Discord Password')
-parser.add_argument('-W', '--waittime', type=int, default=30,
-                    help='Time to wait in seconds for you to load all your notifications')
+# to be added later
+# parser.add_argument('-F', '--folder', type=str, const='images', help="Folder that the images will be saved to")
 args = parser.parse_args()
 
 
@@ -20,8 +23,8 @@ email = args.email
 password = args.password
 
 opts = Options()
-opts.headless = False
-browser = Chrome(options=opts)
+opts.headless = True
+browser = webdriver.Chrome(ChromeDriverManager().install(), options=opts)
 browser.get('https://discord.com/login')
 email_element = browser.find_element_by_name('email')
 email_element.send_keys(email)
@@ -34,7 +37,7 @@ password_element.submit()
 
 print('Successfully logged in')
 
-time.sleep(5)
+time.sleep(10)
 
 actions = ActionChains(browser)
 
@@ -46,17 +49,35 @@ inbox = buttons[1]
 actions.move_to_element(inbox
                         ).click().perform()
 
-time.sleep(args.waittime)
+time.sleep(2)
+
+while True:
+    try:
+        load_more = browser.find_element_by_class_name('lookFilled-yCfaCM')
+        load_more.click()
+        time.sleep(2)
+    except:
+        break
 
 
 mentions_list = browser.find_elements_by_class_name('container-iA3Qrz')
-
+previous = open('previous.txt', 'r').read()
+is_first = True
 for mention in mentions_list:
     message = mention.find_element_by_class_name(
         'messageContent-2t3eCI')
     message = message.text
+
     if message.find('Dreamt') != 0 or message.find('Dreamt') == -1:
         continue
+    if message == previous:
+        break
+    if is_first:
+        f = open('previous.txt', 'w')
+        f.write(message)
+        f.close()
+        is_first = False
+
     first_quote = message.index('"')
     second_quote = message.index('"', first_quote + 1)
     prompt = message[first_quote +
@@ -69,5 +90,9 @@ for mention in mentions_list:
         seeds = [message[message.index('-S')+2:len(message)]]
 
     images = mention.find_elements_by_class_name('originalLink-Azwuo9')
+    print(f'Downloading {prompt}')
     for i, image in enumerate(images):
         download_discord_file(image.get_attribute('href'), prompt, seeds[i])
+
+
+print('Downloads sucessfuly finished')
