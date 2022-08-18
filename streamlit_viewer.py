348d73d1029378650a6b1dc18be3074b9fb27d30
Andrew Mead <ammead@umass.edu>
2022-08-18 14:11:26 -0400

    Add single column option, download button, and update gitignore

--- streamlit_viewer.py
+++ streamlit_viewer.py
@@ -8,6 +8,7 @@ prompts = os.listdir('images')
 
 search = st.text_input('Search propmpts', '')
 show_imgs = st.checkbox('Show all images/ disable dropdowns', value=False)
+use_cols = st.checkbox('Use columns', value=True)
 
 st.markdown(
     """
@@ -25,19 +26,31 @@ for prompt in prompts:
         folder = os.walk('images/' + prompt)
         if not show_imgs:
             expander = st.expander(prompt)
-            col1, col2 = expander.columns(2)
+            if use_cols:
+                col1, col2 = expander.columns(2)
         else:
             st.header(prompt)
-            col1, col2 = st.columns(2)
+            if use_cols:
+                col1, col2 = st.columns(2)
         is_col1 = True
         for dir, _, imgs in folder:
             for img in imgs:
                 if img == 'grid.png':
                     continue
-                if is_col1:
-                    col1.image(os.path.join(dir, img))
-                    col1.write('Seed: {}'.format(img[:len(img)-4]))
+                if use_cols:
+                    if is_col1:
+                        col1.image(os.path.join(dir, img))
+                        col1.write('Seed: {}'.format(img[:len(img)-4]))
+                        col1.download_button(label='Download', data = open(os.path.join(dir, img), 'rb').read(),
+                                             file_name = prompt + '.png', mime='image/png')
+                    else:
+                        col2.image(os.path.join(dir, img))
+                        col2.write('Seed: {}'.format(img[:len(img)-4]))
+                        col2.download_button(label='Download', data = open(os.path.join(dir, img), 'rb').read(),
+                                             file_name = prompt + '.png', mime='image/png')
+                    is_col1 = not is_col1
                 else:
-                    col2.image(os.path.join(dir, img))
-                    col2.write('Seed: {}'.format(img[:len(img)-4]))
-                is_col1 = not is_col1
+                    st.image(os.path.join(dir, img))
+                    st.write('Seed: {}'.format(img[:len(img)-4]))
+                    st.download_button(label='Download', data = open(os.path.join(dir, img), 'rb').read(),
+                                       file_name = prompt + '.png', mime='image/png')
